6d631de6ae618e9b6c41b0b6ad8ee7c8cfa0e69e
mosmowora <72347633+mosmowora@users.noreply.github.com>
2022-06-12 12:18:54 +0200

    changed sum things

--- base/admin.py
+++ base/admin.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 
 from .models import Room, Topic, Message, User
 
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
 admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(Topic)

7d59e7a9fee307c4f84f251f7938b5dd08db361b
mosmowora <72347633+mosmowora@users.noreply.github.com>
2022-06-12 20:25:24 +0200

    plsss

--- studybud/settings.py
+++ studybud/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production

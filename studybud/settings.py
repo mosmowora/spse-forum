35eb64f476273751239e58871a97b94a7cb07dfa
mosmowora <72347633+mosmowora@users.noreply.github.com>
2022-06-12 20:22:05 +0200

    pls work

--- studybud/settings.py
+++ studybud/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,6 +30,10 @@ DEBUG = True
 
 # hope this helps
 ALLOWED_HOSTS = ['127.0.0.1', 'aeternix-forum.herokuapp.com']
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 
 # Application definition
@@ -150,8 +154,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = STATIC_URL / 'images/'
+STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'

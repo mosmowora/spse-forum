be73d8afe5ab10bcffb0ae3f39d5544c121fb5cf
mosmowora <72347633+mosmowora@users.noreply.github.com>
2023-07-10 11:34:30 +0200

    Revert "Pinning it"
    This reverts commit e7b1390b83b0fef66eeb05f180ded8e3209d737f.

--- base/urls.py
+++ base/urls.py
@@ -20,6 +20,4 @@ urlpatterns = [
 
     path('topics/', views.topicsPage, name="topics"),
     path('activity/', views.activityPage, name="activity"),
-
-    path('error/', views.fallback, name="fallback"),
 ]
--- base/views.py
+++ base/views.py
@@ -9,7 +9,7 @@ from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm, UserForm
 # Create your views here.
 
-def loginPage(request: HttpRequest):
+def loginPage(request):
     page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
@@ -34,7 +34,7 @@ def loginPage(request: HttpRequest):
     return render(request, 'base/login_register.html', {'page': page})
 
 
-def logoutUser(request: HttpRequest):
+def logoutUser(request):
     logout(request)
     return redirect('home')
 
@@ -83,7 +83,7 @@ def pinRoom(request: HttpRequest, pk):
     pinned_room.save()
     return redirect(to="home")
 
-def room(request: HttpRequest, pk):
+def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all()
     participants = room.participants.all()
@@ -102,29 +102,23 @@ def room(request: HttpRequest, pk):
     return render(request, 'base/room.html', context)
 
 
-def userProfile(request: HttpRequest, pk):
-    try:
-        user = User.objects.get(id=pk)
-        rooms = user.room_set.all()
-        room_messages = user.message_set.all()
-        topics = Topic.objects.all()
-        context = {'user': user, 'rooms': rooms,
-                'room_messages': room_messages, 'topics': topics}
-        return render(request, 'base/profile.html', context)
-    except Exception:
-        return fallback(request)
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms,
+               'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
 
-
-def fallback(request: HttpRequest):
-    return render(request, 'base/error-site.html')
 
 @login_required(login_url='login', redirect_field_name=None)
-def createRoom(request: HttpRequest):
+def createRoom(request):
     form = RoomForm()
     topics = Topic.objects.all()
     if request.method == 'POST':
         topic_name = request.POST.get('topic')
-        topic, _ = Topic.objects.get_or_create(name=topic_name)
+        topic, created = Topic.objects.get_or_create(name=topic_name)
 
         Room.objects.create(
             host=request.user,
@@ -139,7 +133,7 @@ def createRoom(request: HttpRequest):
 
 
 @login_required(login_url='login', redirect_field_name=None)
-def updateRoom(request: HttpRequest, pk):
+def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
     topics = Topic.objects.all()
@@ -148,7 +142,7 @@ def updateRoom(request: HttpRequest, pk):
 
     if request.method == 'POST':
         topic_name = request.POST.get('topic')
-        topic, _ = Topic.objects.get_or_create(name=topic_name)
+        topic, created = Topic.objects.get_or_create(name=topic_name)
         room.name = request.POST.get('name')
         room.topic = topic
         room.description = request.POST.get('description')
@@ -160,7 +154,7 @@ def updateRoom(request: HttpRequest, pk):
 
 
 @login_required(login_url='login', redirect_field_name=None)
-def deleteRoom(request: HttpRequest, pk):
+def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
 
     if request.user != room.host:
@@ -173,7 +167,7 @@ def deleteRoom(request: HttpRequest, pk):
 
 
 @login_required(login_url='login')
-def deleteMessage(request: HttpRequest, pk):
+def deleteMessage(request, pk):
     message = Message.objects.get(id=pk)
 
     if request.user != message.user:
@@ -187,7 +181,7 @@ def deleteMessage(request: HttpRequest, pk):
 
 
 @login_required(login_url='login', redirect_field_name=None)
-def updateUser(request: HttpRequest):
+def updateUser(request):
     user = request.user
     form = UserForm(instance=user)
 
@@ -199,12 +193,12 @@ def updateUser(request: HttpRequest):
 
     return render(request, 'base/update-user.html', {'form': form})
 
-def topicsPage(request: HttpRequest):
+def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html', {'topics': topics})
 
 
-def activityPage(request: HttpRequest):
+def activityPage(request):
     room_messages = Message.objects.all()
     return render(request, 'base/activity.html', {'room_messages': room_messages})
